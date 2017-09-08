482b7ef5cd7bcee6998248000afe91bfaf95908c
lucianobragaweb <lucianobragagroairas@gmail.com>
2017-09-08 13:05:42 -0300

    Initial Commit

---
+++ app.js
@@ -0,0 +1,20 @@
+/* jshint esnext: true */
+Vue.filter('upper', (value) => value.toUpperCase());
+
+Vue.filter('limit', (value, limit = 15, dot = '...') => { return value.length <= limit ? value + ' ...' : value.substr(0, limit) + dot; });
+
+new Vue({
+  el: 'body',
+  data: {
+    title: 'Curso Vue.js <smal>!!</smal>',
+    description: 'Primeiro código Vue',
+    active: true,
+    name: 'luciano'
+  },
+  ready(){
+    setTimeout(() => {
+      this.title = 'Curso Vue';
+      this.active = false;
+    }, 3000)
+  }
+});
