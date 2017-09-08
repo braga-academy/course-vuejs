0ab3b6a5bcb3fdb273c6eba32a201639168f604a
lucianobragaweb <lucianobragagroairas@gmail.com>
2017-09-08 17:25:45 -0300

    Radio e checkbox dinâmicos (v-for + v-model)

---
+++ js/v-for-3.js
@@ -0,0 +1,34 @@
+new Vue({
+    el: 'body',
+    data: {
+        states: [
+            {
+                uf: 'CE',
+                name: 'Ceará'
+
+            },
+            {
+                uf: 'BA',
+                name: 'Bahia'
+
+            },
+            {
+                uf: 'SP',
+                name: 'São Paulo'
+
+            },
+            {
+                uf: 'RJ',
+                name: 'Rio de Janeiro'
+
+            }
+        ],
+        colors: [
+            'Azul',
+            'Verde',
+            'Amarelo'
+        ],
+        selected: []
+
+    }
+})
