64dd159c4dafeeee3d9b77578ed2b8b1cd0b337e
lucianobragaweb <lucianobragagroairas@gmail.com>
2017-09-08 16:35:59 -0300

    v-for - Parte 02

---
+++ js/v-for-2.js
@@ -0,0 +1,27 @@
+new Vue({
+    el: 'body',
+    data: {
+        states: [
+            {
+                uf: 'CE',
+                name: 'Ceará'
+
+            },
+            {
+                uf: 'BA',
+                name: 'Bahia'
+
+            },
+            {
+                uf: 'SP',
+                name: 'São Paulo'
+
+            },
+            {
+                uf: 'RJ',
+                name: 'Rio de Janeiro'
+
+            }
+        ]
+    }
+})
